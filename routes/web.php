56b9f3683abbe4cf4626c449a45e1f7f4e241c1e
Agustina Ortiz <aggustinaortiz@gmail.com>
2025-10-20 14:02:01 -0300

    welcome diseño

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
+Route::get('/', function () {
+    if (auth()->check()) {
+        // Si está logueado, mostrar la pantalla de inicio
+        return view('welcome');
+    } else {
+        // Si no está logueado, ir al login
+        return redirect()->route('login');
+    }
+});
+
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
